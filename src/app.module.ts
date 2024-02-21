3dbe1c26fd3a13973ca4863c520ff2e21dfc5e47
libinthomas33 <libinthomas33@gmail.com>
2024-02-21 18:29:35 +0530

    Add student module, create, get all, get student by id apis

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { GraphQLModule } from '@nestjs/graphql';
 import { LessonModule } from './lesson/lesson.module';
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Lesson } from './lesson/lesson.entity';
+import { StudentModule } from './student/student.module';
+import { Student } from './student/student.entity';
 
 @Module({
   imports: [
@@ -13,12 +15,13 @@ import { Lesson } from './lesson/lesson.entity';
       url: 'mongodb://localhost/school',
       synchronize: true,
       useUnifiedTopology: true,
-      entities: [Lesson],
+      entities: [Lesson, Student],
     }),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       autoSchemaFile: 'schema.gql',
     }),
+    StudentModule,
   ],
   controllers: [],
   providers: [],
---
+++ src/student/student.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Student } from './student.entity';
+import { Repository } from 'typeorm';
+import { CreateStudentInput } from './create-student.input';
+import { v4 as uuid } from 'uuid';
+
+@Injectable()
+export class StudentService {
+  constructor(
+    @InjectRepository(Student)
+    private studentRepository: Repository<Student>,
+  ) {}
+
+  getStudents(): Promise<Student[]> {
+    return this.studentRepository.find();
+  }
+
+  getStudent(id): Promise<Student> {
+    return this.studentRepository.findOne({ where: { id } });
+  }
+
+  async createStudent(
+    createStudentInput: CreateStudentInput,
+  ): Promise<Student> {
+    const { firstName, lastName } = createStudentInput;
+    const student = this.studentRepository.create({
+      id: uuid(),
+      firstName,
+      lastName,
+    });
+    return this.studentRepository.save(student);
+  }
+}
